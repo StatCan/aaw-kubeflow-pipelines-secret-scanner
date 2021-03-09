04b5238cd7b60fea94a7f95104e280031f7359ec
Blair Drummond <blair.drummond@canada.ca>
2021-03-09 16:37:44 -0500

    feat: add slack webhook

---
+++ create-slack-secret.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# Create a secret with the slack webhook
+# in a given namespace. E.g
+#
+# > this-script.sh monitoring
+#
+# Will prompt for webhook
+# and will create a secret named `kfp-slack-webhook`
+# `
+
+set -e
+
+NAMESPACE="$1"
+if test -z "$NAMESPACE"; then
+	echo "Error: No namespace provided." >&2
+	echo "Please provide a namespace as an argument." >&2
+	exit 1
+fi
+
+# Read Password
+read -s -p "Slack Webhook: " webhook
+echo
+
+if test -z "$webhook"; then
+	echo "Provided webhook was blank. Exiting." >&2
+	exit 1
+fi
+
+kubectl create secret generic kfp-slack-webhook \
+		--from-literal=SLACK_WEBHOOK=$webhook \
+		--namespace $NAMESPACE
